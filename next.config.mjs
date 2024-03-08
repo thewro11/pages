a5cf9e438350342b72dd5a6cb96f25a36c8bd990
Théo Teenster <thewro11@gmail.com>
2024-03-09 01:27:04 +0700

    change workflows

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   output: "export",
   basePath: "/pages",
-  experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
 };
 
 export default nextConfig;
